953184b381451fe2ad6c7772413d487f8934b086
荒野無燈 <ttys3@outlook.com>
2019-12-11 13:27:24 +0800

    sed: use @ as sep

--- make.sh
+++ make.sh
@@ -10,9 +10,11 @@ URL_ESCPACED=$(echo $URL_ESCPACED | sed 's/"/\\\\"/g')
 #sed must escape & => \&
 URL_ESCPACED=$(echo $URL_ESCPACED | sed 's/&/\\&/g')
 
+URL_ESCPACED=$(echo $URL_ESCPACED | sed 's/@/\\@/g')
+
 # echo $URL_ESCPACED
 
-cat tpn.c | sed "s/::TPL__PLACEHOLDER__/$URL_ESCPACED/" > tmp.c \
+cat tpn.c | sed "s@::TPL__PLACEHOLDER__@$URL_ESCPACED@" > tmp.c \
 && gcc -o./tpn.out -x c ./tmp.c \
 && ./tpn.out \
 && rm -f ./tpn.out \
